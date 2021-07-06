a3b8cab0e50f327cb3f66c5183ff53e0ea4fb68c
sosog <sosogvritishvilI@gmail.com>
2021-07-06 15:05:33 +0400

    nested routes for client added

--- helpers/get-nested-content-routes.js
+++ helpers/get-nested-content-routes.js
@@ -1,6 +1,17 @@
+import fetch from 'node-fetch'
+
 export const getRoutes = async () => {
   const { $content } = require('@nuxt/content')
   const files = await $content({ deep: true }).only(['path']).fetch()
 
-  return files.map((file) => (file.path === '/index' ? '/' : file.path))
+  const contentRoutes = files.map((file) =>
+    file.path === '/index' ? '/' : file.path
+  )
+
+  const data = await fetch(
+    process.env.BASE_URL + '/public/content/helpers/companies.json'
+  ).then((res) => res.json())
+  const companiesRoutes = data.map((company) => `/clients/${company.slug}`)
+
+  return [...contentRoutes, ...companiesRoutes]
 }
